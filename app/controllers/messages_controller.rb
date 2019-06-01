3eeab0f57102aa6fac4ab6fcc5c5c4e2fc83c440
riri0427 <riri190427@gmail.com>
2019-06-01 17:32:31 +0900

    Update messages_controller.rb
    Fix index action to prevent from error

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,8 @@
 class MessagesController < ApplicationController
   def index
     @message = Message.new
-    @group = @message.group
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages.includes(:user)
   end
 
   def create
