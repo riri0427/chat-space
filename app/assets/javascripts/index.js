c9e81582675a97b8e190fd487f6a4dcb08848b9b
riri0427 <riri190427@gmail.com>
2019-06-06 16:35:05 +0900

    Update index.js
    Write ajax

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,4 +3,10 @@ $(function() {
     var input = $("#user-search-field").val();
     
   })
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
 });
