8d76f891465461facdfc00c4650bf409d86418b3
riri0427 <riri190427@gmail.com>
2019-06-06 16:36:16 +0900

    Update index.js
    Define appendUser function and appendErrMsgHTML function

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,19 @@
 $(function() {
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                </div>`
+    $("#add-name").append(html);
+  }
+
+  function appendErrMsgToHTML(msg) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${msg}</p>
+                </div>`
+    $("#add-name").append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     
