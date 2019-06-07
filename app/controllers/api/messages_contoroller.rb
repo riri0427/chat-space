8d3c4f0c889d92173176a1ccfc7f56ce65650ae4
riri0427 <riri190427@gmail.com>
2019-06-07 16:55:16 +0900

    Update messages_controller.rb
    Fix index action

--- app/controllers/api/messages_contoroller.rb
+++ app/controllers/api/messages_contoroller.rb
@@ -1,9 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
     @messages = Message.includes(:user)
-    respond_to do |format|
-      format.html
-      format.json { @new_message = Message.where('id > ?', params[:message][:id])}
-    end
+    @new_message = Message.where('id > ?', params[:message][:id])
   end
 end
