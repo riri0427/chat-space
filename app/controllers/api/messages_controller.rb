94292e515b31072d5751085a0c4ed3302abf907b
riri0427 <riri190427@gmail.com>
2019-06-09 00:04:13 +0900

    Updage api/messages_controller.rb
    Define index action

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,3 +1,6 @@
 class Api::MessagesController < ApplicationController
-  
+  def index
+    group = Group.find(params[:group_id])
+    @new_messages = Message.where('id > ?', params[:id]).where(group_id: group.id)
+  end
 end
