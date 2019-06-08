a56d5bcad1cfb9ea8aae815dc1564b20f18bb029
riri0427 <riri190427@gmail.com>
2019-06-08 23:59:42 +0900

    Delete messages_controller.rb
    Because this file was not loaded

--- app/controllers/api/messages_contoroller.rb
+++
@@ -1,6 +0,0 @@
-class Api::MessagesController < ApplicationController
-  def index
-    @messages = Message.includes(:user)
-    @new_message = Message.where('id > ?', params[:message][:id])
-  end
-end
